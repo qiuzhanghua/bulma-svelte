ab3ab8e9cdf66bb356e389e0b5a53eef46aa2631
qiuzhanghua <qiuzhanghua@icloud.com>
2022-02-17 20:22:07 +0800

    use bulma with svelte

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+    let count = 0
+    const increment = () => {
+        count += 1
+    }
+</script>
+<!--<svelte:head>-->
+<!--    <link rel="stylesheet" href="css/default.css">-->
+<!--</svelte:head>-->
+<button class="button is-primary is-responsive is-outlined is-rounded" on:click={increment}>
+    Clicks: {count}
+</button>
+
+<style lang="scss">
+    @import "../style/default.sass";
+</style>
